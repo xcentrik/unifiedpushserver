cbab53eb70ea54d908df94b709d85a93cc907413
andrijdavid <andrijdavid@hotmail.com>
2016-08-07 17:07:48 +0300

    Fix certificate generation in certificate.sh

--- configuration/certs/certificate.sh
+++ configuration/certs/certificate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export DOMAIN=aerogear.dev
+
 SERVER_PASSWORD=$(head -c 2000 /dev/urandom | tr -dc a-z0-9A-Z | head -c 256)
 
 keytool -genkey -noprompt -alias server.$DOMAIN -keyalg RSA -keysize 2048 -keystore server.$DOMAIN.keystore -storepass $SERVER_PASSWORD -keypass $SERVER_PASSWORD -validity 365 -dname "C=US,ST=North Carolina,O=Red Hat,L=Raleigh,CN=server.aerogear.dev,OU=DEVELOPMENT,emailAddress=aerogear@aerogear.org"
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,6 @@ cd $UPSDIST/migrator/ups-migrator
 
 # replace eventual private certificate
 if [ -d /keys ];then
-    export DOMAIN="aerogear.dev"
     export SERVER_PASSWORD=$(head -c 2000 /dev/urandom | tr -dc a-z0-9A-Z | head -c 256)
     echo "custom certificates directory found - replacing default self signed generated certificate"
     mv $JBOSS_HOME/standalone/configuration/certs/server.$DOMAIN.keystore $JBOSS_HOME/standalone/configuration/certs/selfsigned.keystore
