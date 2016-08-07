45779cc4f332e298a8da7ab6fa1e47f5ea7d9165
andrijdavid <andrijdavid@hotmail.com>
2016-08-07 17:45:28 +0300

    Fix server password on certificate.sh

--- configuration/certs/certificate.sh
+++ configuration/certs/certificate.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-export DOMAIN=aerogear.dev
-
-SERVER_PASSWORD=$(head -c 2000 /dev/urandom | tr -dc a-z0-9A-Z | head -c 256)
+export SERVER_PASSWORD=$(head -c 2000 /dev/urandom | tr -dc a-z0-9A-Z | head -c 256)
 
 keytool -genkey -noprompt -alias server.$DOMAIN -keyalg RSA -keysize 2048 -keystore server.$DOMAIN.keystore -storepass $SERVER_PASSWORD -keypass $SERVER_PASSWORD -validity 365 -dname "C=US,ST=North Carolina,O=Red Hat,L=Raleigh,CN=server.aerogear.dev,OU=DEVELOPMENT,emailAddress=aerogear@aerogear.org"
 
